43e432f64b96172f1d5c872cc6c300f7f346d759
Aslamnh <aslamnh.nh@gmail.com>
2025-05-20 21:37:51 +0700

    biji

---
+++ Project Game Bingo/src/project/game/bingo/ProjectGameBingo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project.game.bingo;
+
+/**
+ *
+ * @author aslam
+ */
+public class ProjectGameBingo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
